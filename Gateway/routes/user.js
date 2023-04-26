1533424d9cb78cfdb3e450cbac9fe916daa5243f
arus18 <nazeer.arus18@gmail.com>
2023-04-26 21:44:34 +0530

    fixes

--- Gateway/routes/user.js
+++ Gateway/routes/user.js
@@ -66,7 +66,7 @@ router.delete('/buyers/:id', async (req, res) => {
 // GET all sellers
 router.get('/sellers', async (req, res) => {
     try {
-      const response = await axios.get(`${Config.USER_SERVICE}/sellers`);
+      const response = await axios.get(`${Config.USER_SERVICE}/seller`);
       res.json(response.data);
     } catch (err) {
       res.status(500).send(err);
@@ -76,7 +76,7 @@ router.get('/sellers', async (req, res) => {
   // GET a single seller by ID
   router.get('/sellers/:id', async (req, res) => {
     try {
-      const response = await axios.get(`${Config.USER_SERVICE}/sellers/${req.params.id}`);
+      const response = await axios.get(`${Config.USER_SERVICE}/seller/${req.params.id}`);
       res.json(response.data);
     } catch (err) {
       res.status(500).send(err);
@@ -86,7 +86,7 @@ router.get('/sellers', async (req, res) => {
   // CREATE a new seller
   router.post('/sellers', async (req, res) => {
     try {
-      const response = await axios.post(`${Config.USER_SERVICE}/sellers`, req.body);
+      const response = await axios.post(`${Config.USER_SERVICE}/seller`, req.body);
       res.json(response.data);
     } catch (err) {
       res.status(500).send(err);
@@ -96,7 +96,7 @@ router.get('/sellers', async (req, res) => {
   // UPDATE a seller by ID
   router.put('/sellers/:id', async (req, res) => {
     try {
-      const response = await axios.put(`${Config.USER_SERVICE}/sellers/${req.params.id}`, req.body);
+      const response = await axios.put(`${Config.USER_SERVICE}/seller/${req.params.id}`, req.body);
       res.json(response.data);
     } catch (err) {
       res.status(500).send(err);
@@ -106,7 +106,7 @@ router.get('/sellers', async (req, res) => {
   // DELETE a seller by ID
   router.delete('/sellers/:id', async (req, res) => {
     try {
-      const response = await axios.delete(`${Config.USER_SERVICE}/sellers/${req.params.id}`);
+      const response = await axios.delete(`${Config.USER_SERVICE}/seller/${req.params.id}`);
       res.json(response.data);
     } catch (err) {
       res.status(500).send(err);
